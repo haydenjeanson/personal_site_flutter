0117c8817e758e1bab84bab13c0e0fc82b386602
Hayden Jeanson <haydenjeanson@gmail.com>
2021-01-06 18:58:28 -0500

    Added colour as a parameter

--- lib/components/background_box.dart
+++ lib/components/background_box.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:personal_site_flutter/contants.dart';
 
+// ignore: must_be_immutable
 class BackgroundBox extends StatelessWidget {
   final double opacity;
   final Widget child;
+  Color colour;
 
-  const BackgroundBox(
-      {this.opacity = kDefaultBoxOpacity, @required this.child});
+  BackgroundBox(
+      {this.opacity = kDefaultBoxOpacity, @required this.child, this.colour}) {
+    if (this.colour == null) {
+      this.colour = kMainColourScheme.background;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Theme.of(context)
-              .colorScheme
-              .background
-              .withOpacity(this.opacity),
+          color: this.colour.withOpacity(this.opacity),
           borderRadius: BorderRadius.circular(8.0)),
       child: this.child,
     );
