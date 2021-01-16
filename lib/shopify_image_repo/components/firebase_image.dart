ee64f0ca2d2784021557a8f11684f0deeca7f398
Hayden Jeanson <haydenjeanson@gmail.com>
2021-01-16 17:26:40 -0500

    correctly loads and stores image sizes from firestore database

--- lib/shopify_image_repo/components/firebase_image.dart
+++ lib/shopify_image_repo/components/firebase_image.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
-
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:personal_site_flutter/constants.dart';
 import 'package:personal_site_flutter/util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseImage extends StatefulWidget {
   final Reference image;
@@ -21,11 +23,41 @@ class _FirebaseImageState extends State<FirebaseImage> {
   final Reference image;
   _FirebaseImageState(this.image);
 
-  FirebaseStorage storage = FirebaseStorage.instance;
+  static const String anonymousUser = 'anonymous';
 
-  // 100 is an abitrary number > 0 so that something still appears if it errors
-  double naturalWidth = 100;
-  double naturalHeight = 100;
+  FirebaseStorage storage = FirebaseStorage.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  String imageName;
+  Map<String, double> heightMap = {};
+  Map<String, double> widthMap = {};
+
+  String _getCurrentUser() {
+    User user = FirebaseAuth.instance.currentUser;
+    if (user.isAnonymous) {
+      return anonymousUser;
+    } else {
+      return user.uid;
+    }
+  }
+
+  @override
+  void initState() {
+    this.imageName = this.image.fullPath.toString().split('/')[1];
+
+    firestore
+        .collection('users')
+        .doc(_getCurrentUser())
+        .collection('images')
+        .doc(this.imageName)
+        .get()
+        .then((value) {
+      this.heightMap[value.id] = value.data()['height'];
+      this.widthMap[value.id] = value.data()['width'];
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,34 +116,30 @@ class _FirebaseImageState extends State<FirebaseImage> {
       // This is not a real error -> It works when built but incorrectly
       // displaying this error is a known issue of dart:ui
       // ignore: undefined_prefixed_name
-      ui.platformViewRegistry.registerViewFactory(image.toString(), (_) {
-        ImageElement img = ImageElement()..src = url;
-
-        this.naturalWidth = (img.naturalWidth) as double;
-        this.naturalHeight = (img.naturalHeight) as double;
-
-        return img;
+      ui.platformViewRegistry.registerViewFactory(ref.fullPath.toString(), (_) {
+        return ImageElement()..src = url;
       });
     });
 
     return HtmlElementView(
-      viewType: image.toString(),
+      viewType: ref.fullPath.toString(),
     );
   }
 
-  OverlayEntry _imageOverlay(HtmlElementView image) {
+  OverlayEntry _imageOverlay(HtmlElementView htmlImage) {
     OverlayEntry overlay;
-    if (image != null) {
-      // Scale image to size that fits the screen
-      while ((this.naturalHeight > MediaQuery.of(context).size.height) ||
-          (this.naturalWidth > MediaQuery.of(context).size.width)) {
-        this.naturalHeight /= 1.1;
-        this.naturalWidth /= 1.1;
+    if (htmlImage != null) {
+      // Scale htmlImage to size that fits the screen
+      while ((this.heightMap[this.imageName] >
+              MediaQuery.of(context).size.height) ||
+          (this.widthMap[this.imageName] > MediaQuery.of(context).size.width)) {
+        this.heightMap[this.imageName] /= 1.1;
+        this.widthMap[this.imageName] /= 1.1;
       }
     } else {
-      // Set size of Image not found container
-      this.naturalWidth = MediaQuery.of(context).size.width / 3;
-      this.naturalHeight = MediaQuery.of(context).size.height / 3;
+      // Set size of 'Image not found' container
+      this.widthMap[this.imageName] = MediaQuery.of(context).size.width / 3;
+      this.heightMap[this.imageName] = MediaQuery.of(context).size.height / 3;
     }
 
     overlay = OverlayEntry(builder: (context) {
@@ -122,12 +150,16 @@ class _FirebaseImageState extends State<FirebaseImage> {
             color: Colors.black.withOpacity(kDefaultBoxOpacity),
           ),
           Positioned(
-            left: (MediaQuery.of(context).size.width - this.naturalWidth) / 2,
-            top: (MediaQuery.of(context).size.height - this.naturalHeight) / 2,
-            width: this.naturalWidth,
-            height: this.naturalHeight,
-            child: image != null
-                ? image
+            left: (MediaQuery.of(context).size.width -
+                    this.widthMap[this.imageName]) /
+                2,
+            top: (MediaQuery.of(context).size.height -
+                    this.heightMap[this.imageName]) /
+                2,
+            width: this.widthMap[this.imageName],
+            height: this.heightMap[this.imageName],
+            child: htmlImage != null
+                ? htmlImage
                 : Container(
                     alignment: Alignment.center,
                     color: Theme.of(context).colorScheme.secondary,
