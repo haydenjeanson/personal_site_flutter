8dc344b1e63e7dd7a6a02261e994b745c50a4b61
Hayden Jeanson <haydenjeanson@gmail.com>
2021-01-18 15:57:14 -0500

    name_container now navigates home

--- lib/components/name_container.dart
+++ lib/components/name_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:personal_site_flutter/components/background_box.dart';
 import 'package:personal_site_flutter/constants.dart';
+import 'package:personal_site_flutter/screens/welcome_screen.dart';
+import 'package:personal_site_flutter/util.dart';
 
 class NameContainer extends StatefulWidget {
   final String text;
@@ -31,43 +34,51 @@ class _NameContainerState extends State<NameContainer> {
   Widget build(BuildContext context) {
     return Hero(
       tag: kNameHeroTag,
-      child: BackgroundBox(
-        opacity: this.opacity,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Container(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: FittedBox(
-                    child: Text(
-                      this.text,
-                      style: kNavTextStyle,
+      child: ElevatedButton(
+        onPressed: () {
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            Navigator.pushNamed(context, WelcomeScreen.kID);
+          });
+        },
+        style: invisibleButton(),
+        child: BackgroundBox(
+          opacity: this.opacity,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: FittedBox(
+                      child: Text(
+                        this.text,
+                        style: kNavTextStyle,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                alignment: Alignment.centerRight,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(8.0),
-                    bottomRight: Radius.circular(8.0),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(8.0),
+                      bottomRight: Radius.circular(8.0),
+                    ),
+                    image: new DecorationImage(
+                        alignment: Alignment.topCenter,
+                        image: _imageToShow,
+                        fit: BoxFit.cover),
                   ),
-                  image: new DecorationImage(
-                      alignment: Alignment.topCenter,
-                      image: _imageToShow,
-                      fit: BoxFit.cover),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
